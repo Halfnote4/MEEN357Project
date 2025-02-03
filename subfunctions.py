5d91ef19d393226aaf47dc9ae3c43dd39088cb2d
esilvatamu <166772487+esilvatamu@users.noreply.github.com>
2025-02-03 13:55:07 -0600

    Update subfunctions.py
    Added my assigned functions to code and segmented one dict. from rover

--- subfunctions.py
+++ subfunctions.py
@@ -36,6 +36,13 @@ rover = {
     }
 }
 
+speed_reducer= {
+    'type': 'reverted',
+    'diam_pinion': 0.04,  # m
+    'diam_gear': 0.07,    # m
+    'mass': 1.5           # kg
+}
+
 # Planet constants
 planet = {
     'g': 3.72  # m/s^2 (acceleration due to gravity)
@@ -45,6 +52,7 @@ planet = {
 
 
 import numpy as np
+import scipy
 
 
 def tau_dcmotor(omega, motor):
@@ -68,16 +76,82 @@ def tau_dcmotor(omega, motor):
 
 
 
-# def get_gear_ratio(speed_reducer):
+def get_gear_ratio(speed_reducer):
+    if type(speed_reducer) is not dict:
+        raise Exception('<Speed reducer input is not a dict>')
+    
+    pinion = rover['wheel_assembly']['speed_reducer']['diam_pinion']
+    gear = rover['wheel_assembly']['speed_reducer']['diam_gear']
+    
+    typeo = rover['wheel_assembly']['speed_reducer']['type']
+    
+    if typeo is not 'reverted':
+        raise Exception('<Invalid type called')
+    
+    Ng = (gear/pinion)**2
 
-#     return Ng
+    return Ng
 
-# def get_mass(rover):
+def get_mass(rover):
 
-#     return m
+    '''
+This function computes rover mass in kilograms. It accounts for the chassis, power subsystem, science payload,
+and six wheel assemblies, which itself is comprised of a motor, speed reducer, and the wheel itself.
+    '''
+    
+    if type(rover) is not dict:
+        raise Exception('<Rover input is not a dict>')
+    
+    m_chassey = rover['chassis']['mass']
+    m_payload = rover['science_payload']['mass']
+    m_power = rover['power_subsys']['mass']
+    
+    m_wheel = rover['wheel_assembly']['wheel']['mass']  
+    m_reducer = rover['wheel_assembly']['speed_reducer']['mass'] 
+    m_motor = rover['wheel_assembly']['motor']['mass']     
+    
+    m_wheelass = m_wheel + m_reducer + m_motor
+    
+    m = 6 * m_wheelass + m_chassey + m_power + m_payload
 
-# def F_rolling(omega, terrain_angle, rover, planet, Crr):
-#     return
+    return m
+
+def F_rolling(omega, terrain_angle, rover, planet, Crr):
+    
+    #a/c - verify scalars or vectors of same size and dicts
+    
+    if (type(omega) is not int) and (type(omega) is not isinstance(omega, np.ndarray)):
+        raise Exception('<omega is not vector or scalar>')      
+    if (type(terrain_angle) is not int) and (type(terrain_angle) is not isinstance(terrain_angle, np.ndarray)):
+        raise Exception('<terrain_angle is not vector or scalar>')  
+    if type(rover) is not dict:
+        raise Exception('<Rover input is not a dict>')
+    if type(planet) is not dict:
+        raise Exception('<Planet input is not a dict>')        
+    if (type(Crr) is not int) and Crr < 0 :
+        raise Exception('<Crr is not a positive scalar>')
+    
+    #b - verify bounds
+    
+    if terrain_angle < -75 and terrain_angle > 75:
+        raise Exception('<Terrain_angle not in bounds')    
+    
+    Fn = get_mass(rover)*planet['g']*np.cos(np.degrees(terrain_angle))
+    
+    Frr_simple = Crr*Fn
+    
+    omega_out = omega/get_gear_ratio(rover)
+    
+    v_rover = rover['wheel_assembly']['wheel']['radius']*omega_out
+    
+    Frr = scipy.erf(40*v_rover)*Frr_simple
+    
+    return Frr
+
+
+
+
+
 
 # def F_gravity(terrain_angle, rover, planet):
 #     return Fgt
