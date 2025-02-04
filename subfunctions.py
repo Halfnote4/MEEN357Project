87999c6389f67886a918da1d574da686eaa942a5
edossani <eimaanidossani@tamu.edu>
2025-02-04 15:19:59 -0600

    Update subfunctions.py

--- subfunctions.py
+++ subfunctions.py
@@ -6,41 +6,28 @@ all necessary functions for Part 1 of Project for MEEN 357 - Spring 2025
 
 # Constants for Mars Rover Calculations
 # Nested dictionary of the rover
-speed_reducer= {
-    'type': 'reverted',
-    'diam_pinion': 0.04,  # m
-    'diam_gear': 0.07,    # m
-    'mass': 1.5           # kg
-}
-
-wheel= {
-    'radius': 0.30,  # m
-    'mass': 1.0      # kg
-}
-
-motor= {
-    'torque_stall': 170,    # N·m
-    'torque_noload': 0,     # N·m
-    'speed_noload': 3.80,   # rad/s
-    'mass': 5.0             # kg
-}
-
-chassis= {
-    'mass': 659  # kg
-}
-
-wheel_assembly = {
-    'wheel': wheel,
-    'speed_reducer': speed_reducer,
-    'motor': motor
-}
-
-
 rover = {
-    'wheel_assembly': wheel_assembly
-    ,
-    'chassis': chassis
-    ,
+    'wheel_assembly': {
+        'wheel': {
+            'radius': 0.30,  # m
+            'mass': 1.0      # kg
+        },
+        'speed_reducer': {
+            'type': 'reverted',
+            'diam_pinion': 0.04,  # m
+            'diam_gear': 0.07,    # m
+            'mass': 1.5           # kg
+        },
+        'motor': {
+            'torque_stall': 170,    # N·m
+            'torque_noload': 0,     # N·m
+            'speed_noload': 3.80,   # rad/s
+            'mass': 5.0             # kg
+        }
+    },
+    'chassis': {
+        'mass': 659  # kg
+    },
     'science_payload': {
         'mass': 75  # kg
     },
@@ -49,7 +36,12 @@ rover = {
     }
 }
 
-
+speed_reducer= {
+    'type': 'reverted',
+    'diam_pinion': 0.04,  # m
+    'diam_gear': 0.07,    # m
+    'mass': 1.5           # kg
+}
 
 # Planet constants
 planet = {
@@ -60,7 +52,7 @@ planet = {
 
 
 import numpy as np
-from scipy.special import erf
+import scipy
 
 
 def tau_dcmotor(omega, motor):
@@ -148,7 +140,7 @@ def F_rolling(omega, terrain_angle, rover, planet, Crr):
     
     Frr_simple = Crr*Fn
     
-    omega_out = omega/get_gear_ratio(speed_reducer)
+    omega_out = omega/get_gear_ratio(rover)
     
     v_rover = rover['wheel_assembly']['wheel']['radius']*omega_out
     
@@ -214,7 +206,7 @@ def F_net(omega, terrain_angle, rover, planet, Crr):
 
     # validate all elements of terrain_angle are between -75 and +75 degrees
     if np.any((np.asarray(terrain_angle) < -75) | (np.asarray(terrain_angle) > 75)):
-        raise Exception('<Terrain angle not in bounds>')
+        raise Exception('<Terrain angle not in bounds between -75 and 75 degrees>')
 
     # validate rover and planet are dictionaries
     if type(rover) is not dict:
