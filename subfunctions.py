2f477c5f249c69b06ee159fa48b1f6300622277b
edossani <eimaanidossani@tamu.edu>
2025-02-06 14:58:40 -0600

    Update subfunctions.py

--- subfunctions.py
+++ subfunctions.py
@@ -188,11 +188,11 @@ def F_gravity(terrain_angle, rover, planet):
     return Fgt
 
 
-terrain_angle = np.array([-5.0, 0.0, 5.0, 10.0, 20.0, 30.0]) # degrees!
-Fgt = F_gravity(terrain_angle, rover, planet)
-print('Omega Fgt')
-for i in range(len(Fgt)):
-    print('{:3.4F} {:3.4F}'.format(terrain_angle[i], Fgt[i]))
+#terrain_angle = np.array([-5.0, 0.0, 5.0, 10.0, 20.0, 30.0]) # degrees!
+#Fgt = F_gravity(terrain_angle, rover, planet)
+#print('Omega Fgt')
+#for i in range(len(Fgt)):
+    #print('{:3.4F} {:3.4F}'.format(terrain_angle[i], Fgt[i]))
 
 
 
