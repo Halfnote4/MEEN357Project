235f52182a5b94700482b9be06520b506cb64cc8
esilvatamu <esilva@tamu.edu>
2025-02-07 19:52:45 -0600

    Quasi-completed files
    analysis terrain slope and rolling resistance file updates

--- analysis_rolling_resistance.py
+++ analysis_rolling_resistance.py
@@ -2,4 +2,19 @@ import subfunctions
 import numpy
 
 Crr_array = numpy.linspace(0.01,0.4,25)
+slope_array_deg = numpy.linspace(-10,35,25)
 
+
+
+CRR, SLOPE = numpy.meshgrid(Crr_array, slope_array_deg)
+
+VMAX = numpy.zeros(numpy.shape(CRR), dtype = float)
+
+
+N = numpy.shape(CRR)[0]
+for i in range(N):
+    for j in range(N):
+        Crr_sample = float(CRR[i,j])
+        slope_sample = float(SLOPES[i,j])
+        VMAX[i,j] = ... # here you put code to find the max speed at Crr_sample and
+ # slope_sample
--- analysis_terrain_slope.py
+++ analysis_terrain_slope.py
@@ -1,19 +1,32 @@
 from subfunctions import *
-import numpy
-
-
+import numpy as np
 import scipy.optimize as so
+from scipy.optimize import root_scalar
+import matplotlib.pyplot as plt
 
 #GOAL: Plot of v_max vs slope_array_deg
 
 
 #Assumptions according to specifications
 Crr = 0.2
-slope_array_deg = numpy.linspace(-10,35,25)
+slope_list_deg = np.linspace(-10,35,25)
+omega_max = np.zeros(len(slope_list_deg), dtype=float)
+omega_nl = rover['wheel_assembly']['motor']['speed_noload']
 
-#set vmax as empty array
-vmax = np.zeros(len(slope_array_deg), dtype = float)
 
 #To find vmax -> root of fmax = 0 at each given angle
-for i in range(len(slope_array_deg)):
-    vmax[i] = so.root(F_net(omega,float(slope_array_deg[i]),rover,planet,Crr))
+for ii in range(len(slope_list_deg)):
+    fun = lambda omega: F_net(omega, float(slope_list_deg[ii]), rover, planet, Crr)
+    sol = root_scalar(fun, method='bisect', bracket=[0, omega_nl])
+    omega_max[ii] = rover['wheel_assembly']['wheel']['radius']*(sol.root/get_gear_ratio(rover['wheel_assembly']['speed_reducer']))
+    
+
+#omegain to out with gear ratio -> vel
+
+#plotting vmax vs slope_array_deg
+plt.figure(1)
+plt.plot(slope_list_deg, omega_max)  
+plt.title('Terrain Slope Analysis')
+plt.xlabel('Terrain Angle [degrees]')
+plt.ylabel('Rover Speed [m/s]')
+#plt.show()
